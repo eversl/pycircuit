beef2db583309182a5e202009ae83566d99aca94
Leon Evers <leon@evers.name>
2014-02-08 10:57:33 +0100

    added Vector type, multiplier

--- PyCircuit.py
+++ PyCircuit.py
@@ -3,11 +3,19 @@ Created on Feb 4, 2014
 
 @author: leone
 '''
-import inspect
 
+
+def zip_all(*args):
+    for i in xrange(0, len(args)):
+        for j in xrange(i+1, len(args)):
+            if len(args[i]) != len(args[j]):
+                raise TypeError('Vectors are not of equal size')
+    return zip(*args)
+       
+    
 class Signal():
     def __init__(self, initval = False):
-        self.value = initval
+        self.value = bool(initval)
         self.fanout = []
         
     def __xor__(self, other):
@@ -19,11 +27,14 @@ class Signal():
     def __and__(self, other):
         return And(self, other)
     
-    def __str__(self):
-        return 'Signal({0})'.format(self.value)
-        
+    def __invert__(self):
+        return Not(self)
+    
+    def __mul__(self, other):
+        return Vector([self] * other)
+
     def __repr__(self):
-        return self.__str__()
+        return 'Signal({0})'.format(self.value)
         
     def set(self, value = True):
         if value != self.value:
@@ -42,22 +53,94 @@ class FeedbackSignal(Signal):
         sig.fanout.append(inner)
         inner()
     
+
+class Vector():
+    def __init__(self, value, bits=16):
+        try:
+            value = (s for s in value)
+        except TypeError:
+            self.ls = intToSignals(int(value), bits)
+        else:
+            self.ls = [s if isinstance(s, Signal) else Signal(s) for s in value]
+        
+    def __repr__(self):
+        return 'Vector({0}, {1})'.format(int(self), len(self))
+
+    def __len__(self):
+        return len(self.ls)
+        
+    def __getitem__(self, key):
+        return self.ls[key]
+        
+    def __iter__(self):
+        return iter(self.ls)
+        
+    def __add__(self, other):
+        return Vector(RippleCarryAdder(self, other)[0])
+        
+    def __sub__(self, other):
+        return Vector(RippleCarryAdder(self, -other)[0])
+        
+    def __mul__(self, other):
+        return Vector(Multiplier(self, other))
+        
+    def __floordiv__(self, other):
+        return NotImplemented
+        
+    def __mod__(self, other):
+        return NotImplemented
+        
+    def __divmod__(self, other):
+        return NotImplemented
+        
+    def __lshift__(self, other):
+        return NotImplemented
+        
+    def __rshift__(self, other):
+        return NotImplemented
+        
+    def __and__(self, other):
+        return Vector(s & o for s, o in zip_all(self, other))
+        
+    def __xor__(self, other):
+        return Vector(s ^ o for s, o in zip_all(self, other))
+            
+    def __or__(self, other):
+        return Vector(s | o for s, o in zip_all(self, other))
+        
+    def __neg__(self):
+        inv = ~self
+        return inv + Vector(1, len(self))
+        
+    def __pos__(self):
+        return self
+        
+    def __abs__(self):
+        return Vector(If(self[-1], -self, +self))
+        
+    def __invert__(self):
+        return Vector(~a for a in self)
+    
+    def __int__(self):
+        return signalsToInt(self.ls)
+        
+    def __setitem__(self, key, value):
+        print key, value
+        return self
+
+def intToSignals(num, bits):
+    return [Signal(((num >> i) & 1) == 1) for i in xrange(bits)]
+
+def signalsToInt(ls, signed=True):
+    num = 0
+    for i in xrange(len(ls)):
+        num |= (1 << i if ls[i].value else 0)  
+    if signed and ls[-1].value:
+        num -= (1 << len(ls))
+    return num
     
 
-class Circuit():
-    def __init__(self, name, inputs):
-        self.name = name
-        self.inputs = inputs
-        print 'Circuit', name, inputs
-#         for i in inputs:
-#             inputs[i].fanout.append(self)
-            
-    def __call__(self, *args):
-        print 'call', args
-            
-    def update(self):
-        print 'update', self.name
-        
+
 
 def CircuitOper(func, *args):
     sig = Signal()
@@ -83,6 +166,7 @@ def Not(a):
 def Nor(a, b):
     return CircuitOper(lambda x, y: not (x | y), a, b)
 
+
 def HalfAdder(a, b):
     s = a ^ b
     c = a & b
@@ -94,6 +178,43 @@ def FullAdder(a, b, c_in):
     c_out = c1 | c2
     return s2, c_out
      
+
+def RippleCarryAdder(als, bls, c=Signal()):
+    sls = []
+    for a, b in zip_all(als, bls): 
+        s, c = FullAdder(a, b, c)
+        sls.append(s)
+    return sls, c
+
+def Negate(als):
+    sls, _ = RippleCarryAdder([Not(a) for a in als], [Signal()] * len(als), Signal(True))
+    return sls
+
+def Multiplier(als, bls, signed = True):
+    if signed:
+        als_sign = als[-1]
+        bls_sign = bls[-1]
+        als = If(als_sign, Negate(als), als)
+        bls = If(bls_sign, Negate(bls), bls)
+    else: 
+        als = [a for a in als]  # to make sure Vectors are made to lists
+        bls = [b for b in bls]
+    sls, c = [Signal()] * (len(bls)-1), Signal()
+    for a in als:
+        sls, c = RippleCarryAdder([a & b for b in bls], sls + [c])
+        bls = [Signal()] + bls
+    
+    res = sls + [c]
+    if signed:   
+        sls_sign = als_sign ^ bls_sign
+        res = If(sls_sign, Negate(res), res) 
+    return res
+     
+     
+     
+def If(pred, cons, alt):
+    return [(pred & c) | (Not(pred) & a) for c, a in zip_all(cons, alt)]
+         
      
 def SRLatch(s, r):
     q_fb = FeedbackSignal(not r.value)
--- test.py
+++ test.py
@@ -4,26 +4,15 @@ Created on Feb 6, 2014
 @author: matrix1
 '''
 import unittest
-from PyCircuit import Signal, FullAdder, SRLatch, DLatch, DFlipFlop, Not
+from PyCircuit import Signal, FullAdder, SRLatch, DLatch, DFlipFlop, Not,\
+    intToSignals, signalsToInt, RippleCarryAdder, Negate, Vector, Multiplier
 
 
 class Test(unittest.TestCase):
-
-
-    def test_FullAdder(self):
-        a, b, c = Signal(), Signal(), Signal()
-        signals = FullAdder(a, b, c)
-        
-        self.assertEquals([sig.value for sig in signals], [False, False])
-        a.set()  # sum should be 1 (sum, carry) 
-        self.assertEquals([sig.value for sig in signals], [True, False])
-        b.set() # sum = 2
-        self.assertEquals([sig.value for sig in signals], [False, True])
-        c.set() # sum = 3
-        self.assertEquals([sig.value for sig in signals], [True, True])
-
-
-
+    def test_str(self):
+        self.assertEqual(str(Signal(0)), 'Signal(False)')
+        self.assertEqual(str(Signal(1)), 'Signal(True)') 
+        self.assertEqual(str(Vector(-10)), 'Vector(-10, 16)') 
 
     def test_SRLatch(self):    
         r = Signal()
@@ -80,8 +69,59 @@ class Test(unittest.TestCase):
         clk.reset()
         self.assertEquals([sig.value for sig in signals], [False, True])
  
-    
+    def test_FullAdder(self):
+        a, b, c = Signal(), Signal(), Signal()
+        signals = FullAdder(a, b, c)
+        
+        self.assertEquals([sig.value for sig in signals], [False, False])
+        a.set()  # sum should be 1 (sum, carry) 
+        self.assertEquals([sig.value for sig in signals], [True, False])
+        b.set() # sum = 2
+        self.assertEquals([sig.value for sig in signals], [False, True])
+        c.set() # sum = 3
+        self.assertEquals([sig.value for sig in signals], [True, True])
 
+
+
+    def test_intConversion(self):
+        for i in xrange(-100, 256):
+            ls = intToSignals(i, 9)
+            j = signalsToInt(ls, True)
+            self.assertEqual(i, j)
+
+
+    def test_RippleCarryAdder(self):
+        for a in xrange(-256, 255, 67):
+            for b in xrange(-22756, 32767, 1453):
+                als = intToSignals(a, 16)
+                bls = intToSignals(b, 16)
+                sls, c_out = RippleCarryAdder(als, bls)
+                sum = signalsToInt(sls, True)
+                self.assertEqual(sum, a + b)
+                self.assertEqual(c_out.value, a < 0)
+
+    def test_arith(self):
+        self.assertEquals(int(-Vector(10)), -10)
+        self.assertEquals(int(Vector(10) + Vector(-24)), 10 + -24)
+        self.assertEquals(int(Vector(10) - Vector(24)), 10 - 24)
+        self.assertEquals(int(Vector(0x0f) | Vector(0xf0)), 0x0f | 0xf0)
+        self.assertEquals(int(Vector(0x0e) & Vector(0x03)), 0x0e & 0x03)
+        self.assertEquals(int(Vector(0x0e) ^ Vector(0x07)), 0x0e ^ 0x07)
+        self.assertEquals(int(~Vector(0xff)), ~0xff)
+        self.assertEquals(int(abs(Vector(-5))), abs(-5))
+                    
+    def test_Negate(self):
+        for num in xrange(-22756, 32767, 1453):
+                als = intToSignals(num, 16)
+                sls = Negate(als)
+                neg = signalsToInt(sls, True)
+                self.assertEqual(neg, - num)
+
+
+    def test_Multiplier(self):
+        self.assertEqual(signalsToInt(Multiplier(Vector(15,4), Vector(15,4), False),False), 225)
+    
+    
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
